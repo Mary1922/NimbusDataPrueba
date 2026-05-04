4b806d210234cc80749d62bc68dc2ffb616f4dd8
Mary1922 <saori.kidou2@gmail.com>
2026-05-04 15:25:26 +0200

    Modify logger.py changing order of the directory creation and functions log_info log_error log_warning

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -2,30 +2,37 @@ import logging
 import os
 
 def config_logger(name="nimbus_logger", log_file="logs/app.log", level=logging.INFO):
-    # Asegurar que la carpeta logs existe
+ 
+    # 1. Create logger first to avoid multiple handlers if called multiple times
+    logger = logging.getLogger(name)
+    logger.setLevel(level)   
+
+    # 2. Avoid duplicating handlers if they are called multiple times
+    if logger.handlers:
+        return logger
+
+    # 3. Create log directory if it doesn't exist
     os.makedirs(os.path.dirname(log_file), exist_ok=True)
 
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-
-    # Evitar duplicar handlers si se llama varias veces
-    if not logger.handlers:
-        formatter = logging.Formatter(
-            "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+    # 4. Formatter
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
         )
+    
+     # 5. Handlers
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(formatter)
 
-        file_handler = logging.FileHandler(log_file)
-        file_handler.setFormatter(formatter)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
 
-        console_handler = logging.StreamHandler()
-        console_handler.setFormatter(formatter)
-
-        logger.addHandler(file_handler)
-        logger.addHandler(console_handler)
+    # 6. Add handlers
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
 
     return logger
 
-# Inicialización
+# Initialize the logger
 _logger = config_logger()
 
 def log_info(message):
